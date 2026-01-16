d961603c155e79b1bcfe68c06c09e27cc4b47990
Aashish Shah <aashishsah005@gmail.com>
2026-01-16 10:56:05 +0530

    commit

--- WT_Lecture/post.php
+++ WT_Lecture/post.php
@@ -4,6 +4,16 @@
     <title>Post Example</title>
 </head>
 <body>
-    <form method="POST" action="">
+    <form method="POST" action="post_2.php">
+        Enrollment No.: 
+        <input type="text" name="enroll" required/><Br>
+        Name : 
+        <input type="text" name="name" required/><Br>
+        Batch : 
+        <input type="text" name="batch" required/><Br>
+        Class : 
+        <input type="text" name="class" required/><Br>
+        <input type="Submit" />
+    </form>
 </body>
 </html>
---
+++ WT_Lecture/post_2.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Enrollment No.: ".$_POST['enroll']."<br>";
+    echo "Name : ".$_POST['name']."<br>";
+    echo "Batch : ".$_POST['batch']."<br>";
+    echo "Class : ".$_POST['class']."<br>";
+?>
